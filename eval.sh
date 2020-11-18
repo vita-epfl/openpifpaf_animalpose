175eb888c5ef622e9e1b2ac8436d89ca88a66093
Lorenzo <lorenzobertoni9@gmail.com>
2020-11-18 16:42:21 +0100

    add sbatch scripts

---
+++ eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 20
+#SBATCH --mem 96G
+#SBATCH --time 06:00:00
+#SBATCH --account=vita
+#SBATCH --gres gpu:1
+
+pattern=$1
+shift
+
+srun singularity exec --bind /scratch/izar/bertoni --nv ../pytorch_latest.sif \
+  /bin/bash -c "source ../.venv/apollo/bin/activate && find outputs/ -name \"$pattern\" -exec python3 -m openpifpaf.eval --checkpoint {} $(printf "%s " "$@") \;"
---
+++ train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 40
+#SBATCH --mem 96G
+#SBATCH --time 72:00:00
+#SBATCH --account=vita
+#SBATCH --gres gpu:2
+
+srun singularity exec --bind /scratch/izar/bertoni --nv ../pytorch_latest.sif \
+  /bin/bash -c "source ../.venv/apollo/bin/activate && time CUDA_VISIBLE_DEVICES=0,1 python3 -u -m openpifpaf.train $(printf "%s " "$@")"
