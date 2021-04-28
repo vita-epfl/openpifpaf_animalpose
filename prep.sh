4bec8bce450bbb6f72712813aebb5ae38e2b0623
Lorenzo <lorenzobertoni9@gmail.com>
2021-04-28 14:15:10 +0200

    update

--- prep.sh
+++ prep.sh
@@ -3,9 +3,10 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 40
 #SBATCH --mem 96G
-#SBATCH --time 1:00:00
+#SBATCH --time 10:00:00
 #SBATCH --account=vita
+#SBATCH --gres gpu:1
 
 module load gcc python cuda
 source ../.venv/animal/bin/activate
-python -m openpifpaf_animalpose.voc_to_coco
+python3 -m openpifpaf_animalpose.voc_to_coco
