a271209586bf0c0ce821a7d6aebbb5db72affb63
Lorenzo <lorenzobertoni9@gmail.com>
2021-04-28 11:09:46 +0200

    update shell

--- train.sh
+++ train.sh
@@ -3,9 +3,9 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 40
 #SBATCH --mem 96G
-#SBATCH --time 72:00:00
+#SBATCH --time 48:00:00
 #SBATCH --account=vita
 #SBATCH --gres gpu:2
 
 srun singularity exec --bind /scratch/izar --nv ../pytorch_latest.sif \
-  /bin/bash -c "source ../.venv/apollo/bin/activate && time CUDA_VISIBLE_DEVICES=0,1 python3 -u -m openpifpaf.train $(printf "%s " "$@")"
+  /bin/bash -c "source ../.venv/animal/bin/activate && time CUDA_VISIBLE_DEVICES=0,1 python3 -u -m openpifpaf.train $(printf "%s " "$@")"
